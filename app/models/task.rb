36f84c6e425b54b304e09906e80e5026ac85f3a6
nadinantire <nadinantire@gmail.com>
2019-09-27 06:58:52 +0000

    correct error step 12

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,14 +3,14 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :content, presence: true
   validates :content, length: {minimum: 2, maximum: 20}
-  validate :start_datetime_cannot_be_later_than_end_datetime
+  # validate :start_datetime_cannot_be_later_than_end_datetime
   enum priority: [:low, :medium, :hight]
-  private
-  def start_datetime_cannot_be_later_than_end_datetime
-    if start_date > end_date
-      errors.add(:start_datetime, "Can not be set after the end date and time")
-    end
+  # private
+  # def start_datetime_cannot_be_later_than_end_datetime
+  #   if start_date > end_date
+  #     errors.add(:start_datetime, "Can not be set after the end date and time")
+  #   end
  
-  end
+  # end
   paginates_per 3
 end
